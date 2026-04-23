cac46d862d3b73df28133e269658982727bf8cdb
Arifdsaf <you@example.com>
2026-04-23 04:28:20 +0100

    Update settings for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -116,8 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+ALLOWED_HOSTS = ['costlens-lgrv.onrender.com', '127.0.0.1', 'localhost']
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
